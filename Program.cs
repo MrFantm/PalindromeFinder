cb6f6116d4b225843cb469b34d9163e61d58d113
Carter Dunaway <81333581+MrFantm@users.noreply.github.com>
2024-07-16 19:54:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PalindromFinder {
+    class Program {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Enter a string of text: ");
+            string input = Console.ReadLine();
+
+            if(IsPalindrome(input)) 
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else 
+            {
+                Console.WriteLine("Not a Palindrome");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static bool IsPalindrome(string input) 
+        {
+            string cleanedInput = Regex.Replace(input, "[^a-zA-Z0-9]", "").ToLower();
+            char[] text = cleanedInput.ToCharArray();
+            Array.Reverse(text);
+
+            string reverse = new string(text);
+            return reverse == cleanedInput;
+        }
+    }
+}
